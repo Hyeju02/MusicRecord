826d619aaa6842c421dacf5d2b40b2526c923590
2yoona <thisisprofita@gmail.com>
2023-06-06 14:32:40 +0900

    유튜브 url 완료

--- app/src/main/java/com/example/musicrecording/ShowDiaryActivity.java
+++ app/src/main/java/com/example/musicrecording/ShowDiaryActivity.java
@@ -12,12 +12,12 @@ import androidx.appcompat.app.AppCompatActivity;
 
 public class ShowDiaryActivity extends AppCompatActivity implements View.OnClickListener {
     //(오류)다이어리 화면에 표시된 Song, Singer Text 활용해 검색 키워드로 사용
-    /*
-    TextView songtextView = (TextView)findViewById(R.id.Text_Song);
-    TextView singertextView = (TextView)findViewById(R.id.Text_Singer);
-    String songname = songtextView.getText().toString();
-    String singername = singertextView.getText().toString(); */
 
+    TextView songtextView;
+    TextView singertextView;
+    String songname;
+    String singername;
+    ImageButton Btn_PlayDiaryUrl;
 
 
     @Override
@@ -25,8 +25,14 @@ public class ShowDiaryActivity extends AppCompatActivity implements View.OnClick
         super.onCreate(savedInstanceState);
         setContentView(R.layout.showdiary_activity);
 
+        //텍스트뷰 스트링으로 가져오기(song, singer)
+        songtextView = (TextView)findViewById(R.id.Text_Song);
+        singertextView = (TextView)findViewById(R.id.Text_Singer);
+        songname = songtextView.getText().toString();
+        singername = singertextView.getText().toString();
+
         //유튜브 링크 재생 버튼
-        ImageButton Btn_PlayDiaryUrl = (ImageButton) findViewById(R.id.Btn_PlayDiaryUrl);
+        Btn_PlayDiaryUrl = (ImageButton) findViewById(R.id.Btn_PlayDiaryUrl);
         Btn_PlayDiaryUrl.setOnClickListener(this);
 
     }
@@ -35,14 +41,12 @@ public class ShowDiaryActivity extends AppCompatActivity implements View.OnClick
     public void onClick(View v){
         switch (v.getId())
         {
-            //(위 string 해결되어야) 재생버튼 키워드(노래+가수) 검색된 유튜브 화면 열기
-            /*
             case R.id.Btn_PlayDiaryUrl:
+                //스트링 검색어로 넣은 유튜브 창 열기(노래 제목 + 가수 이름 검색된 화면)
                 Toast.makeText(getApplicationContext(), "Show you video", Toast.LENGTH_LONG).show();
                 Intent urlintent = new Intent(Intent.ACTION_VIEW, Uri.parse("https://www.youtube.com/results?search_query=" + songname + " " + singername));
                 startActivity(urlintent);
                 finish();
-                */
         }
     }
 
